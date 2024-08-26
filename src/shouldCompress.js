2e25a0e27b14fd314d312c1afb349fe1976dd5b9
Rushs321 <fyggh566@tutamail.com>
2024-08-26 11:47:20 +0530

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -1,19 +1,25 @@
-const MIN_COMPRESS_LENGTH = 512; // Adjust the minimum compress length as desired
+"use strict";
+
+const MIN_COMPRESS_LENGTH = 1024;
 const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
 
 function shouldCompress(req) {
   const { originType, originSize, webp } = req.params;
 
-  if (!originType.startsWith('image')) {
-    return false;
-  }
-  if (originSize === 0) {
-    return false;
-  }
-  if (webp && originSize < MIN_COMPRESS_LENGTH) {
-    return false;
-  }
-  if (!webp && (originType.endsWith('png') || originType.endsWith('gif')) && originSize < MIN_TRANSPARENT_COMPRESS_LENGTH) {
+  // Convert originSize to a number and default to 0 if invalid
+  const size = parseInt(originSize, 10) || 0;
+
+  if (!originType || !originSize) return false; // Ensure originType and originSize are defined
+
+  if (!originType.startsWith('image')) return false;
+  if (size === 0) return false;
+  if (req.headers.range) return false;
+  if (webp && size < MIN_COMPRESS_LENGTH) return false;
+  if (
+    !webp &&
+    (originType.endsWith('png') || originType.endsWith('gif')) &&
+    size < MIN_TRANSPARENT_COMPRESS_LENGTH
+  ) {
     return false;
   }
 
